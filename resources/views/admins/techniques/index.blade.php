de3f5e10e5504118e74d0eaef8ea4c3bf91bc899
Denis Alekhin <dennyaa2003@gmail.com>
2023-04-17 00:02:19 +0500

    feat: add technique page

---
+++ resources/views/admins/techniques/index.blade.php
@@ -0,0 +1,56 @@
+@extends('templates.admin')
+
+@section('title', 'Admin panel')
+
+@section('style')
+<style>
+.card-img {
+    width: 80px;
+    height: 80px;
+}
+</style>
+@endsection
+
+@section('content')
+<!--Main layout-->
+<main style="margin-top: 58px">
+    <div class="container pt-4 content">
+        <div class="create-container d-flex justify-right mb-3">
+            <a class="btn btn-primary me-3" href="{{ route('admin.techniques.create') }}">
+                Создать
+            </a>
+        </div>
+        @include('inc.flash')
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Название</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse((old('techniques') ?? $techniques) as $technique)
+                <tr class="align-middle">
+
+                    <td>{{ $technique->name }}</td>
+                    <td>
+                        <a href="{{ route('admin.techniques.edit', $technique->id) }}"
+                            class="btn btn-primary p2 flex-fill">Редактировать</a>
+                    </td>
+                    <td>
+                        <form method="POST" action="{{ route('admin.techniques.destroy', $technique->id) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger p2 w-100">Удалить</button>
+                        </form>
+                    </td>
+                </tr>
+                @empty
+                <p>Стили отсутствуют</p>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+</main>
+@endsection
