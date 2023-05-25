39f81541e0cfe777fba689e0d8511a105e665e3d
Denis Alekhin <dennyaa2003@gmail.com>
2023-05-25 15:23:10 +0500

    feat: add payment service

---
+++ app/Service/PaymentService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Service;
+
+use YooKassa\Client;
+
+class PaymentService
+{
+    public function getClient(): Client
+    {
+        $client = new Client();
+        $client->setAuth(config('services.yookassa.shop_id'), config('services.yookassa.secret_key'));
+
+        return $client;
+    }
+    
+    public function createPayment(float $amount, string $description, array $options = []){
+        $client = $this->getClient();
+        $payment = $client->createPayment([
+            'amount' => [
+                'value' => $amount,
+                'currency' => 'RUB'
+            ],
+            'capture' => false,
+            'confirmation' => [
+                'type' => 'redirect',
+                'return_url' => route('user.orders.index'),
+            ],
+            'metadata' => [
+                'transaction_id' => $options['transaction_id'],
+            ],
+            'description' => $description
+        ], uniqid('', true));
+
+        return $payment->getConfirmation()->getConfirmationUrl();
+    }
+}
