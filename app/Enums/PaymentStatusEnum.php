dacdc35aa157e29240ed839a59afab1d127c23d9
Denis Alekhin <dennyaa2003@gmail.com>
2023-05-25 15:04:49 +0500

    feat: add payment status enum

---
+++ app/Enums/PaymentStatusEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+class PaymentStatusEnum
+{
+    public const CREATE = 'CREATE';
+    public const FAILED = 'FAILED';
+    public const CONFIRMED = 'CONFIRMED';
+    
+}
