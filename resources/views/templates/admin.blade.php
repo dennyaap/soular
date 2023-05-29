663dc7e34e27a1c8ad8afc4fe7f0594240391518
Denis Alekhin <dennyaa2003@gmail.com>
2023-05-29 16:13:34 +0500

    fix: delete script

--- resources/views/templates/admin.blade.php
+++ resources/views/templates/admin.blade.php
@@ -162,46 +162,6 @@
     </header>
     <!--Main Navigation-->
     @yield('content')
-
-    <script>
-    var ctx = document.getElementById("myChart");
-
-    var myChart = new Chart(ctx, {
-        type: "line",
-        data: {
-            labels: [
-                "Sunday",
-                "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday",
-                "Friday",
-                "Saturday",
-            ],
-            datasets: [{
-                data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
-                lineTension: 0,
-                backgroundColor: "transparent",
-                borderColor: "#007bff",
-                borderWidth: 4,
-                pointBackgroundColor: "#007bff",
-            }, ],
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: false,
-                    },
-                }, ],
-            },
-            legend: {
-                display: false,
-            },
-        },
-    });
-    </script>
-
 </body>
 
 </html>
